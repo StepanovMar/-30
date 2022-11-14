bcb05f0e7d6adaed231eda9bfbc82fd2415244ed
maste <maste@DESKTOP-N7TT9HL>
2022-11-15 00:45:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write($"Введите число от 1 до 6:");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int i = 0;
+for (i = 1; i < num; i++)
+{
+    if (num % i == 0)
+        sum += i;
+}
+if (sum == num)
+    Console.WriteLine($"число { num} — совершенное");
+else
+    Console.WriteLine($"число { num} — НЕ совершенное");
